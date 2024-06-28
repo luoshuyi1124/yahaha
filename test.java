5935fcf016e1ee95150b6aab4af21ca23d616c23
Kevin Li <luoshuyi1124@gmail.com>
2024-06-27 21:05:41 -0400

    wahoo

--- test.java
+++ test.java
@@ -28,10 +28,51 @@ public class test {
 
         System.out.println(Math.max(x, y));
 
-
-        String stringshenanigans = "The fitnezz gram pacer test...";
-        System.out.println(stringshenanigans);
+        System.out.println("---------- string stuff below");
         
+    //string stuff like methods
+        String stringshenanigans = "The fitnezz gram pacer test...";
+
+        //upper and lower case
+        System.out.println(stringshenanigans.toUpperCase());
+        System.out.println(stringshenanigans.toLowerCase());
+
+        //length
+        System.out.println("string length is: " + stringshenanigans.length());
+
+        //indexOf
+        System.out.println(stringshenanigans.indexOf("gram")); //prints index of first letter of "gram"
+
+        //replace
+        System.out.println(stringshenanigans.replace("zz", "ss"));
+
+    //following returns booleans
+        //isEmpty
+        String sure = "am i empty";
+        boolean booLean = sure.isEmpty();
+        System.out.println(booLean);
+
+        //startsWith and endsWith; looks at whole sentence and accounts for upper/lower case
+        System.out.println(stringshenanigans.startsWith("T"));
+        System.out.println(stringshenanigans.endsWith("s"));
+
+        //contains
+        System.out.println(stringshenanigans.contains("pacer"));
+
+        //charAt
+        System.out.println(stringshenanigans.charAt(15));
+
+        //substring; returns everything after that index
+        System.out.println(stringshenanigans.substring(15));
+
+        System.out.println("---------- casting below");
+
+    //casting (converting data types between each other, like int and float)
+        int loan = 370;
+        float interestRate = 0.05134f;
+        
+        float interest = loan * interestRate;
+        System.out.println(interest);
 
     }
 }
